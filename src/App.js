cc5f6acf15ca1082b94c42b736f1b81bd04dda9d
pooja pandit <102018934+Ppuj@users.noreply.github.com>
2024-11-22 18:28:16 +0530

    Day2 tasks1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Text from './components/Text'
+import Button from './components/Button';
+import InputText from './components/InputText';
+import { useState } from 'react';
+
 
 function App() {
+  let Label='Click me'
+  const [name,setname]=useState('')
+  const [displayname,setdisplayname]=useState('')
+  function onClick(){
+    setdisplayname(name)
+    setname(' ')
+    console.log('button clicked')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld/>
+      <InputText placeholder={'Enter your Name'} value={name} handleChange={setname}/>
+      <Button label={Label} onClick={onClick}/>
+      <Text message={displayname}/>
     </div>
   );
 }
